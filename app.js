bd3094544cae14fe5539e924b966d14c8645f314
Shuprovo Sikder <ssikder20@stuy.edu>
2020-10-21 04:01:09 -0400

    added routers in app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require("./routes/index");
 var loginRouter = require("./routes/login");
 var registerRouter = require("./routes/register");
 var itemRouter = require("./routes/item");
+var cartRouter = require("./routes/cart");
 
 var app = express();
 dotenv.config();
@@ -33,6 +34,7 @@ app.use("/", indexRouter);
 app.use("/", loginRouter);
 app.use("/", registerRouter);
 app.use("/", itemRouter);
+app.use("/", cartRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
