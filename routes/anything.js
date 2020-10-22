f5ecf9596d0f12b9191b2dd0d3c074768773f692
Shuprovo Sikder <ssikder20@stuy.edu>
2020-10-22 19:43:14 -0400

    update anything.js

--- routes/anything.js
+++ routes/anything.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 router.post("/anything", async (req, res, next) => {
   const image = req.body.image;
   console.log(image);
+  res.send("Gotten.");
 });
 
 module.exports = router;
