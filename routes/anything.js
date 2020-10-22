1b63c6b9835583337f382699a469f8d6b5127ee2
Shuprovo Sikder <66190617+ssikder20@users.noreply.github.com>
2020-10-22 19:31:15 -0400

    Update anything.js

--- routes/anything.js
+++ routes/anything.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+// General purpose router just for testing
 router.post("/anything", async (req, res, next) => {
   const image = req.body.image;
   console.log(image);
